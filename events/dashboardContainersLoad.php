ab810350f4e945b2c3b76f54d01991f0a089114f
stefan <stefan@stefan-tuxedo>
2025-10-18 19:17:55 +0200

    update module for v5.3

--- events/dashboardContainersLoad.php
+++ events/dashboardContainersLoad.php
@@ -6,12 +6,7 @@ final class dashboardContainersLoad extends \fpcm\module\event {
 
     public function run() : \fpcm\module\eventResult
     {
-        if (version_compare(\fpcm\classes\loader::getObject('\fpcm\model\system\config')->system_version, '4.3.0-rc9', '<')) {
-            return $this->data;
-        }
-
         $this->data[] = '\fpcm\modules\nkorg\polls\models\dashContainerRecentPoll';
-
         return (new \fpcm\module\eventResult())->setData($this->data);
     }
 
